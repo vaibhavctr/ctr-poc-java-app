4c28b8c4ea24e2563003a4ff1e8e836376ecb47f
vaibhav mishra <v-vaibhav.mishra@circlek.com>
2024-11-04 22:33:16 -0700

    updated function.java

---
+++ src/main/java/com/example/Function.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+public class Function {
+    @FunctionName("hello")
+    public HttpResponseMessage run(
+        @HttpTrigger(name = "req", methods = {HttpMethod.GET}, authLevel = AuthorizationLevel.FUNCTION)
+        HttpRequestMessage<Optional<String>> request,
+        final ExecutionContext context
+    ) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+        String responseMessage = "Hello, Azure!";
+        return request.createResponseBuilder(HttpStatus.OK).body(responseMessage).build();
+    }
+}
